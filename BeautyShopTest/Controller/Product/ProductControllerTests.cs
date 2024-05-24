8ef01129428c9e56effdfb5092dff7a77a8875da
Sepideh Amani <52592900+sepidbarfi@users.noreply.github.com>
2024-05-24 18:11:24 +0430

    Implement SiteSide ProductController Tests

---
+++ BeautyShopTest/Controller/Product/ProductControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using BeautyShopApplication.Services.Interface;
+using BeautyShopDomain.DTOs;
+using BeautyShopWebAPI.Controllers;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautyShopTest.Controller.Product
+{
+    public class ProductControllerTests
+    {
+        private readonly IProductService _productService;
+        private readonly ProductController _productController;
+        public ProductControllerTests()
+        {
+            _productService = A.Fake<IProductService>();
+            _productController = new ProductController(_productService);
+        }
+
+        [Fact]
+        public async Task ProductController_GetProduct_ReturnsProduct()
+        {
+            //Arrange
+            int productId = 1;
+            ProductPageDTO product = A.Fake<ProductPageDTO>();
+            A.CallTo(() => _productService.GetProductPageDTO(productId,CancellationToken.None)).Returns(product);
+
+            //Act
+            var result = await _productController.GetProduct(productId);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("Test")]
+        public async Task ProductController_GetListOfProducts_ReturnsListOfProducts(string? searchString)
+        {
+            //Arrange
+            ProductListRequestDTO productListRequestDTO = A.Fake<ProductListRequestDTO>();
+            ICollection<ProductBoxDTO> productListDTO = A.Fake<ICollection<ProductBoxDTO>>();
+            A.CallTo(() => _productService.GetListOfProductDTOs(searchString, productListRequestDTO, CancellationToken.None))
+                .Returns(productListDTO);
+
+            //Act
+            var result = await _productController.GetListOfProducts(searchString, productListRequestDTO, CancellationToken.None);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+
+        }
+    }
+}
